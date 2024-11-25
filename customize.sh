4afa398fa3e0c5fe4b20c982cef48cefebcdeced
Unicorn369 <3038572935@qq.com>
2024-11-25 09:44:01 +0800

    arm

--- customize.sh
+++ customize.sh
@@ -1,9 +1,11 @@
 # check android
-if [ "$ARCH" != "arm64" ]; then
-  ui_print "! 不支持设备架构: $ARCH"
-  abort "! 仅支持架构 arm64"
-else
+if [ "$ARCH" = "arm" ]; then
   ui_print "- 设备架构: $ARCH"
+elif [ "$ARCH" = "arm64" ]; then
+  ui_print "- 设备架构: $ARCH"
+else
+  ui_print "! 不支持设备架构: $ARCH"
+  abort "! 仅支持架构: arm、 arm64"
 fi
 
 if [ "$API" -lt 26 ]; then
@@ -13,5 +15,13 @@ else
   ui_print "- 设备SDK: $API"
 fi
 
-ui_print "- 安装完成！"
+if [ "$ARCH" = "arm" ]; then
+  ui_print "- 正在移动arm架构文件..."
+  mv -f "$MODPATH/arm/jq" "$MODPATH/system/xbin"
+  mv -f "$MODPATH/arm/lpac-hw" "$MODPATH/system/xbin"
+fi
+
+rm -r "$MODPATH/arm"
+
+ui_print "- 安装完成！！！"
 ui_print "- 重启设备后，在终端模拟器中，执行“lpac h”即可食用"
