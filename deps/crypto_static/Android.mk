3323bd91112d369f3ebb5ea5e2aecde8777993a3
Unicorn369 <3038572935@qq.com>
2024-12-02 11:29:43 +0800

    BoringSSL
    io.github.vvb2060.ndk » boringssl

---
+++ deps/crypto_static/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := crypto_static
+LOCAL_SRC_FILES := ./libs/android.$(TARGET_ARCH_ABI)/libcrypto_static.a
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(PREBUILT_STATIC_LIBRARY)
