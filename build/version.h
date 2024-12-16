38bb810eb53aadcb4d0d00ac7a5d2d6fdb2692c8
Unicorn369 <3038572935@qq.com>
2024-12-16 15:00:42 +0800

    update submodule

--- build/version.h
+++ build/version.h
@@ -1,4 +1,14 @@
 #ifndef LPAC_VERSION_H_
 #define LPAC_VERSION_H_
-#define LPAC_VERSION "v2.1.0-android"
+#if defined(__arm__)
+    #define LPAC_VERSION "v2.1.0-android-arm"
+#elif defined(__aarch64__)
+    #define LPAC_VERSION "v2.1.0-android-arm64"
+#elif defined(__i386__)
+    #define LPAC_VERSION "v2.1.0-android-x86"
+#elif defined(__x86_64__)
+    #define LPAC_VERSION "v2.1.0-android-x86_64"
+#else
+    #define LPAC_VERSION "v2.1.0"
+#endif
 #endif  /* LPAC_VERSION_H_ */
