3f649bcdaecf4127e820db447a187d055c68ea38
Unicorn369 <3038572935@qq.com>
2024-12-16 18:37:24 +0800

    add PCSC

--- customize.sh
+++ customize.sh
@@ -14,14 +14,24 @@ if [ "$API" -lt 26 ]; then
 else
   ui_print "- 设备SDK: $API"
 fi
-
+#
+PCSC_DRIVERS="/data/adb/pcsc/drivers/ifd-ccid.bundle/Contents"
+mkdir -p "$PCSC_DRIVERS/Linux"
+mv -f "$MODPATH/deps/Info.plist" "$PCSC_DRIVERS/"
+#
 if [ "$ARCH" = "arm" ]; then
-  ui_print "- 正在移动arm架构文件..."
-  mv -f "$MODPATH/arm/jq" "$MODPATH/system/xbin"
-  mv -f "$MODPATH/arm/lpac-hw" "$MODPATH/system/xbin"
+  ui_print "- 正在复制ARM架构文件..."
+  mv -f "$MODPATH/deps/arm/jq" "$MODPATH/system/xbin"
+  mv -f "$MODPATH/deps/arm/lpac-hw" "$MODPATH/system/xbin"
+  mv -f "$MODPATH/deps/arm/pcscd" "$MODPATH/system/xbin"
+  ui_print "- 正在复制PCSC驱动文件..."
+  mv -f "$MODPATH/deps/arm/libccid.so" "$PCSC_DRIVERS/Linux/"
+else
+  ui_print "- 正在复制PCSC驱动文件..."
+  mv -f "$MODPATH/deps/arm64/libccid.so" "$PCSC_DRIVERS/Linux/"
 fi
 
-rm -r "$MODPATH/arm"
+rm -rf "$MODPATH/deps"
 
 ui_print "- 安装完成！！！"
 ui_print "- 重启设备后，在终端模拟器中，执行“lpac h”即可食用"
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+MODDIR=${0%/*}
+MODSDIR=$(dirname "$MODDIR")
+
+rm -rf /data/adb/pcsc
