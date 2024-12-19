0fec234f88ace655941e682dfa8d6c9541778ba8
Unicorn369 <3038572935@qq.com>
2024-12-19 13:48:15 +0800

    libccid

--- libpcsclite.mk
+++ libpcsclite.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-PCSC_CFLAGS := \
+PCSC_CFLAGS := -DSIMCLIST_NO_DUMPRESTORE \
   -DUSE_IPCDIR=\"/data/adb/pcsc\" \
   -DPCSCLITE_HP_DROPDIR=\"/data/adb/pcsc/drivers\"
 ########## libpcsclite ##########
@@ -7,17 +7,16 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libpcsclite
 
-LOCAL_CFLAGS := $(PCSC_CFLAGS)
-LOCAL_CFLAGS += -DLIBPCSCLITE -DSIMCLIST_NO_DUMPRESTORE
+LOCAL_CFLAGS := $(PCSC_CFLAGS) -DLIBPCSCLITE
 
 LOCAL_SRC_FILES := \
-	libpcsclite/debug.c \
-	libpcsclite/error.c \
-	libpcsclite/simclist.c \
-	libpcsclite/sys_unix.c \
-	libpcsclite/utils.c \
-	libpcsclite/winscard_clnt.c \
-	libpcsclite/winscard_msg.c
+    libpcsclite/debuglog.c \
+    libpcsclite/error.c \
+    libpcsclite/simclist.c \
+    libpcsclite/sys_unix.c \
+    libpcsclite/utils.c \
+    libpcsclite/winscard_clnt.c \
+    libpcsclite/winscard_msg.c
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/libpcsclite/include \
@@ -52,9 +51,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := pcscd
 LOCAL_STATIC_LIBRARIES := libusb
 
-LOCAL_CFLAGS := $(PCSC_CFLAGS)
+LOCAL_CFLAGS := $(PCSC_CFLAGS) -DPCSCD
 LOCAL_CFLAGS += \
-    -DPCSCLITE_CONFIG_DIR=\"/data/adb/pcsc/etc/reader.conf.d\" \
+    -DPCSCLITE_CONFIG_DIR=\"/data/adb/pcsc/reader.conf.d\" \
     -DOPENCT_FILE=\"/data/adb/pcsc/openct/status\" \
     -DPROFILE_FILE=\"/data/adb/pcsc/pcsc_profile\"
 
@@ -82,10 +81,35 @@ LOCAL_SRC_FILES := \
     libpcsclite/winscard_msg.c \
     libpcsclite/winscard_msg_srv.c \
     libpcsclite/winscard.c \
-    libpcsclite/winscard_svc.c \
+    libpcsclite/winscard_svc.c
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/libpcsclite/include \
     $(LOCAL_PATH)/libpcsclite/include/PCSC
 
 include $(BUILD_EXECUTABLE)
+########## libccid ##########
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libccid
+LOCAL_LDLIBS := -llog
+LOCAL_STATIC_LIBRARIES := libpcsclite libusb
+LOCAL_CFLAGS := $(PCSC_CFLAGS)
+
+LOCAL_SRC_FILES := \
+    libpcsclite/tokenparser.c \
+    libpcsclite/libccid/ccid.c \
+    libpcsclite/libccid/ccid_usb.c \
+    libpcsclite/libccid/commands.c \
+    libpcsclite/libccid/ifdhandler.c \
+    libpcsclite/libccid/strlcpy.c \
+    libpcsclite/libccid/utils.c \
+    \
+    libpcsclite/libccid/openct/buffer.c \
+    libpcsclite/libccid/openct/checksum.c \
+    libpcsclite/libccid/openct/proto-t1.c \
+    \
+    libpcsclite/libccid/towitoko/atr.c \
+    libpcsclite/libccid/towitoko/pps.c
+
+include $(BUILD_SHARED_LIBRARY)
