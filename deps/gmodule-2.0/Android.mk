f44190984c2dfe80bbfe93880e13b413177a08d5
Unicorn369 <3038572935@qq.com>
2026-09-14 14:21:02 +0800

    gmodule-2.0[2.78.3-beta-2]
    com.viliussutkus89.ndk.thirdparty » glib2-ndk25-static

---
+++ deps/gmodule-2.0/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gmodule-2.0
+LOCAL_SRC_FILES := ./libs/android.$(TARGET_ARCH_ABI)/libgmodule-2.0.a
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+$(call import-add-path,$(LOCAL_PATH)/..)
